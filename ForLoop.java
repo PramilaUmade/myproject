17466c6588b0555157b555ad1d08dd8804851a0e
PramilaUmade <133891859+PramilaUmade@users.noreply.github.com>
2023-05-17 20:36:51 +0530

    Java working notebook

---
+++ ForLoop.java
@@ -0,0 +1,12 @@
+package first_project;
+
+public class ForLoop 
+{
+	public static void main(String[] args)
+	{
+		// For loop example
+		for (int i =0; i <= 5; i +=1)
+			{System.out.println(i);
+			}
+		}
+}
